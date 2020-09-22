e1328d309ad0520d01d439a5fccfbe2f741a9cb8
Dennis Soroka <dnis.soroka@gmail.com>
2020-09-22 03:49:38 +0300

    Added tools for extracting raw bytes

---
+++ IpfsManager/Tools/ByteTools.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ipfs.Manager.Tools
+{
+    static class ByteTools
+    {
+        public static List<byte> GetByteFromFile(string path)
+        {
+            List<byte> bytes = new List<byte>();
+            try
+            {
+                using (var fs = new System.IO.FileStream(path, System.IO.FileMode.Open))
+                {
+                    bool isEndOfStream = false;
+                    while (!isEndOfStream)
+                    {
+                        int b = fs.ReadByte();
+                        if (b != -1)
+                        {
+                            bytes.Add((byte)b);
+                        }
+                        else
+                        {
+                            isEndOfStream = true;
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new AggregateException("Unknown error encountered", e);
+            }
+            return bytes;
+        }
+    }
+}
